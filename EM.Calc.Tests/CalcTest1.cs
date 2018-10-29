51c5bffa676d76be2bbdfe5e1d8e89c4d769c6e2
Максим Новиков <pylliok1@mail.ru>
2018-10-29 16:46:08 +0300

    Добавил проект с тестами и ядром

---
+++ EM.Calc.Tests/CalcTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EM.Calc.Tests
+{
+    [TestClass]
+    public class CalcTest1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //Arrange
+            var calc = new Core.Calc();
+            var Sum = 10;
+            //act   
+            var result = calc.Sum(new[] { 5, 3, 2 });
+            //Assent
+            Assert.AreEqual(Sum, result);
+        }
+    }
+}
---
+++ EM.Calc.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EM.Calc.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EM.Calc.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ebbab96-1b5b-4ce1-8c0b-35f84f91bc22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
